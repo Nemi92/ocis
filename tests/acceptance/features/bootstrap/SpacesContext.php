ac9199720b503fe68e78540a6fe08be35b73a0b0
Viktor Scharf <scharf.vi@gmail.com>
2024-02-15 13:04:36 +0100

    share item with share permission if resharing is disabled

--- tests/acceptance/features/bootstrap/SpacesContext.php
+++ tests/acceptance/features/bootstrap/SpacesContext.php
@@ -2189,6 +2189,11 @@ class SpacesContext implements Context {
 			"role" => $rows["role"]
 		];
 
+    // share with custom permission
+    if (isset ($rows["permissions"])){ 
+      $body["permissions"] = $rows["permissions"];
+    }
+
 		$fullUrl = $this->baseUrl . $this->ocsApiUrl;
 		$response = $this->sendPostRequestToUrl(
 			$fullUrl,
